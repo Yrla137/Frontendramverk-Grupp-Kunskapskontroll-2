3f62bf2a1064b4e4892cbd4f411f283389f4b82a
Yrla137 <julia.bjorling@hotmail.com>
2026-05-21 21:31:30 +0200

    Adding states from cutom hook and loading / error to SearchResults

--- src/components/search/SearchResults.jsx
+++ src/components/search/SearchResults.jsx
@@ -1,9 +1,31 @@
+import { useNavigate } from "react-router-dom";
+import ErrorMessage from "./ErrorMessage";
+import LoadingSpinner from "./LoadingSpinner";
 
-const SearchResults = () => {
+const SearchResults = ({
+  filteredData,
+  hasSearched,
+  errorSearch,
+  loadingSearch,
+  onRetry,
+  isLoggedIn
+}) => {
 
+  const navigate = useNavigate();
+
+  if (loadingSearch) {
+  return <LoadingSpinner message="Searching though space..." />;
+  }
+
+  if (errorSearch) {
+    return (
+        <ErrorMessage
+        error={errorSearch}
+        onRetry={onRetry} />
+    );
+  }
   return (
     <section>
-        <h2>Search Results</h2>
     </section>
   )
 }
